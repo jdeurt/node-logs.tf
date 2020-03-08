c858a97fbf4e54ae462b15f86dd2fa18e56cfa48
Juan de Urtubey <jdeurt@gmail.com>
2020-03-07 20:32:10 -0600

    :o

---
+++ src/types/Tf2Class.ts
@@ -0,0 +1,3 @@
+type Tf2Class = "scout" | "soldier" | "pyro" | "demoman" | "heavyweapons" | "engineer" | "medic" | "sniper" | "spy";
+
+export default Tf2Class;
---
+++ src/types/Tf2Team.ts
@@ -0,0 +1,3 @@
+type Tf2Team = "Red" | "Blue";
+
+export default Tf2Team;
